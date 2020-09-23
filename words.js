c08efcf7c5c1ad442717f6ca8c78742189d8ddee
Luis Cabrera Benito <parzibyte@gmail.com>
2020-09-23 12:04:23 -0500

    Cambios menores
    Cambiando tipografía; haciendo funcionar botón de menú y encerrando estilos en un archivo

--- words.js
+++ words.js
@@ -1,3 +1,11 @@
+// Tomado de https://parzibyte.me/blog/2019/06/26/menu-responsivo-bootstrap-4-sin-dependencias/
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector("#menu"),
+        botonMenu = document.querySelector("#botonMenu");
+    if (menu) {
+        botonMenu.addEventListener("click", () => menu.classList.toggle("show"));
+    }
+});
 const LOCAL_STORAGE_WORDS_KEY = "words";
 const getWords = () => (JSON.parse(localStorage.getItem(LOCAL_STORAGE_WORDS_KEY)) || []);
 const saveWords = (words) => (localStorage.setItem(LOCAL_STORAGE_WORDS_KEY, JSON.stringify(words)));
